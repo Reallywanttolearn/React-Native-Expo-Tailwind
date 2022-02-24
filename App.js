a7610147bc0087ff341015f1301eeb97c28a5586
themas <themasfebrianto13@gmail.com>
2022-02-24 21:23:32 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex-1 mt-5 items-center`}>
+      <Image source={logo} style={tw`h-40 w-96`} />
+      <View style={tw`bg-blue-400 py-20 px-5`}>
+        <Text style={tw`text-white text-3xl`}>
+          ini adalah aplikasi android menggunakan React Expo
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
