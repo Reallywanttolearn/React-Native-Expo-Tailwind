9d1d37563d9826ae190cc08dfc5a0996f22cbec2
themas <themasfebrianto13@gmail.com>
2022-02-24 21:41:11 +0700

    coba-coba

--- App.js
+++ App.js
@@ -5,13 +5,19 @@ import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={tw`flex-1 mt-5 items-center`}>
+    <View style={tw`mt-10 items-center`}>
       <Image source={logo} style={tw`h-40 w-96`} />
       <View style={tw`bg-blue-400 py-20 px-5`}>
         <Text style={tw`text-white text-3xl`}>
           ini adalah aplikasi android menggunakan React Expo
         </Text>
       </View>
+      <View style={tw`bg-blue-400 py-20 px-8`}>
+        <Text style={tw`text-white text-3xl`}>
+          belajar instalasi tailwind "twrnc" di React Native
+        </Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
